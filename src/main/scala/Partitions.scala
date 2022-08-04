b8d347c269477c690c2a58543a80e44049f3db5b
hijpax <hij3200@gmail.com>
2022-08-04 15:20:16 -0600

    Sampling done for better dataset management

--- src/main/scala/Partitions.scala
+++ src/main/scala/Partitions.scala
@@ -5,24 +5,12 @@ import Reader.readDF
 object Partitions extends App {
 
   //October 2019
-  val octDF = readDF("2019-Oct.csv")
+  val eventsDF = readDF("2019-*.csv")
 
-  octDF.select("*")
-    .where(to_date(col("event_time"),"yyyy-MM-dd HH:mm:ss z") < to_date(lit("2019-10-02 00:00:00 UTC"),"yyyy-MM-dd HH:mm:ss z"))
-    .limit(200)
+  eventsDF.sample(0.1)
     .write
     .format("csv")
     .mode(SaveMode.Overwrite)
-    .save("src/main/resources/data/2019-october-02")
-
-
-  val febDF = readDF("2020-Feb.csv")
-
-  febDF.select("*")
-    .where(to_date(col("event_time"),"yyyy-MM-dd HH:mm:ss z") < to_date(lit("2020-02-02 00:00:00 UTC"),"yyyy-MM-dd HH:mm:ss z"))
-    .limit(200)
-    .write
-    .format("csv")
-    .mode(SaveMode.Overwrite)
-    .save("src/main/resources/data/2020-february-02")
+    .option("header","true")
+    .save("C:/data/samples")
 }
